df80a4ca502451b14586402db79700ecbd589121
Unnie Ayilliath <unnikannan.ayilliath@opcw.org>
2022-05-14 12:18:19 +0200

    added edit button for customers view

--- front-end/public/js/customers.js
+++ front-end/public/js/customers.js
@@ -12,26 +12,31 @@ function getUsers() {
 }
 var Userheaders = ["User Name", "Firstname", "Lastname", "Email", "isAdmin"];
 function displayUsers(users) {
-    var out = "<h1> Users </h1><table border=1 width=100%>";
+    var html = `<h1> Users </h1>
+                <table border=1 width=100%>
+                <tr style="font-size: 20px;" >`;
     var i;
-    out += '<tr style="font-size: 20px;" >';
     for (i = 0; i < Userheaders.length; i++) {
-        out += '<th >' + Userheaders[i] + '</th>';
+        html += `<th >${Userheaders[i]}</th>`;
     }
-    out += "</tr>";
+    html += "</tr>";
     for (i = 0; i < users.length; i++) {
-        out += "<tr>";
-        out += '<td>' + users[i].username + '</td>';
-        out += '<td>' + users[i].firstname + '</td>';
-        out += '<td>' + users[i].lastname + '</td>';
-        out += '<td>' + users[i].email + '</td>';
-        out += '<td>' + users[i].isAdmin + '</td>';
-        out += "</tr>";
+        html += `<tr>
+                <td>${users[i].username}</td>
+                <td>${users[i].firstname}</td>
+                <td>${users[i].lastname}</td>
+                <td>${users[i].email}</td>
+                <td>${users[i].isAdmin}</td>
+                <td><button onclick="editUser('${users[i].username}')">Edit</button></td>
+                <td></td>
+                </tr>`;
     }
-    out += '<tfoot><tr><td style="background:#000;color:#fff"><b>Total</b></td><td colspan="5"><b>'+users.length+'</b></td></tr></tfoot>';
-    out += '</table>';
-    out += '<br>';
-    out += '<button style="background:#ff6900;color:#fff;padding:5px;border:none;font-size:large" onclick="checkout()">Checkout</button><br>';
-    out+='<div id="cartmessage"></div>';
-    document.getElementById("divCustomers").innerHTML = out;
+    html += `<tfoot><tr><td style="background:#000;color:#fff"><b>Total</b></td><td colspan="5"><b>${users.length}</b></td></tr></tfoot>
+            </table>
+            <br>`;
+    document.getElementById("divCustomers").innerHTML = html;
+}
+
+function editUser(username){
+    window.open(`myaccount.html?user=${username}`,"_blank");
 }
